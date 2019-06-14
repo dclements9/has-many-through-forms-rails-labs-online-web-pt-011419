d63307526129f83a4a96b1fd3edde5f16042c11a
dclements9 <dclements@protonmail.com>
2019-06-14 16:24:52 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: "Auto")
+Category.create(name: "Music")
+User.create(username: "user", email: "email@.com")
